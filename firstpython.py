7adaedb07f2041cd9fce1e6fbbd5a6a9913df93c
anirudhpatil95 <121297107+anirudhpatil95@users.noreply.github.com>
2022-12-23 18:27:22 +0530

    Create firstpython.py
    That's right, I got no commitment issues bitch!

---
+++ firstpython.py
@@ -0,0 +1,4 @@
+# This is the first Python code that I've written in GitHub
+
+print("What's up ma homies! We're gonna Data Science this bitch yo!")
+print("Jesus Ethan, what's with the hood accent?")
